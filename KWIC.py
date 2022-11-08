59a4b58c854387da94bcab784f6003272a09c632
Azamature1719 <ismagulovazamat.g@gmail.com>
2022-11-08 14:23:51 +0400

    Magic constants deleted

--- KWIC.py
+++ KWIC.py
@@ -5,6 +5,10 @@ alphabetized_index = list()
 id_start_line = list() # list of start postions of lines
 id_end_line = list()   # list of end position of lines
 
+LINE_DELIMETER = '\n'
+WORD_DELIMETER = ' '
+LINE_NUM = 0           # index element = (line_num, word_position)    
+WORD_POSITION = 1      # constants for identification an index element  
 
 def input_strings():
     global characters, id_start_line, id_end_line
@@ -17,7 +21,7 @@ def input_strings():
 
     # add to the line_index starting indexes of all the lines
     for i in range(len(characters)):
-        if(characters[i] == '\n'):
+        if(characters[i] == LINE_DELIMETER):
             id_start_line.append(i+1)
 
     # create a list of end indexes of each line
@@ -29,17 +33,18 @@ def circular_shift():
     global index
 
     line_id = 0
+    char_id = 0
 
     # add location of the first word in the first line
-    index.append((0, 0))
+    index.append((line_id, char_id))
 
-    # create index - list of tuples (line_id, char_id)    
-    for char_id in range(len(characters)):
-        if(characters[char_id] == ' '):  # word is found
+    while(char_id < len(characters)):
+        if(characters[char_id] == WORD_DELIMETER):  # word is found
             index.append((line_id, char_id + 1))
-        if(characters[char_id] == '\n'): # new line is found
+        if(characters[char_id] == LINE_DELIMETER): # new line is found
             line_id += 1
             index.append((line_id, char_id + 1))
+        char_id += 1
 
 
 # create shifited phrases from index:
@@ -48,17 +53,17 @@ def circular_shift():
 # | mom and dad --> mom and dad (all the line - static_part)
 # mom | and dad --> and dad mom 
 # mom and | dad --> dad mom and 
-
 def create_shift_from_index(_index, _id):
-    static_part_start = _index[_id][1]
-    static_part_end = id_end_line[_index[_id][0]]
+    static_part_start = _index[_id][WORD_POSITION]
+    static_part_end = id_end_line[_index[_id][LINE_NUM]]
 
-    shift_part_start = id_start_line[_index[_id][0]]
-    shift_part_end = _index[_id][1]
+    shift_part_start = id_start_line[_index[_id][LINE_NUM]]
+    shift_part_end = _index[_id][WORD_POSITION]
 
     static_part = characters[static_part_start : static_part_end]
     shift_part = characters[shift_part_start : shift_part_end]
     
+    # Add ' ' to the end of the last line 
     if(_index[_id][0] == len(id_end_line) - 1):
         static_part += ' '
     
@@ -71,7 +76,7 @@ def alphabetizing():
     global characters, alphabetized_index, id_end_line
 
     # replace '\n' in lines to create one-line shifted phrase
-    characters = characters.replace('\n', ' ')
+    characters = characters.replace(LINE_DELIMETER, WORD_DELIMETER)
 
     # create dict({index : shift}) for further sorting indexes by values
     alph_dict = dict()
@@ -100,4 +105,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
